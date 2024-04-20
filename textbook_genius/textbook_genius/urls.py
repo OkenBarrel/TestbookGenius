fb9d165743b2a9e3dd2ce739e94d31c1bfdd4a37
Milky <theshiretherivendell@outlook.com>
2024-04-20 17:40:13 +0800

    refactor: add meterial ui with dependency (not tested yet)

--- textbook_genius/textbook_genius/urls.py
+++ textbook_genius/textbook_genius/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('api/',include('api.urls')),
+    path('api/',include('api.urls')),
     #path('',include('frontend.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
     
