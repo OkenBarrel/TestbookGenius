d32e356b1112a1ae9a45995661d67630e14c439b
PengYe <1480019539@qq.com>
2024-07-04 11:29:57 +0800

    feat: update UpdateBookPage.js

--- textbook_genius/frontend/reactapp/src/components/HomePage.js
+++ textbook_genius/frontend/reactapp/src/components/HomePage.js
@@ -2,6 +2,7 @@ import {Button,DialogContent,Grid,Typography,TextField} from '@mui/material';
 import React,{Component} from "react";
 import CreateBookPage from './CreateBookPage';
 import Book from "./Book"
+import UpdateBookPage from './UpdateBookPage';
 import LoginPage from './LoginPage';
 import {
     BrowserRouter as Router,
@@ -17,6 +18,7 @@ function HomePage(props) {
         <div>
           <h2>This is HomePage</h2>
           <Button variant='contained' to="/create" component={Link}>创建书籍</Button>
+          <Button variant='contained' to="/update" component={Link}>修改书籍信息</Button>
           <Button variant='contained' to="/register" component={Link}>注册</Button>
           <Button variant='contained' to="/login" component={Link}>登录</Button>
           <Button variant='contained' to="/user/:userId" component={Link}>用户信息</Button>
@@ -30,6 +32,7 @@ function HomePage(props) {
           <Route path="/" element={renderHomePage()} />
           <Route path="/create" element={<CreateBookPage/>} />
           <Route path="/book/:isbn" element={<Book/>} />
+          <Route path="/update" element={<UpdateBookPage/>} />
           <Route path="/register" />
           <Route path="/login" element={<LoginPage/>}/>
           <Route path="/user/:userId"/>
